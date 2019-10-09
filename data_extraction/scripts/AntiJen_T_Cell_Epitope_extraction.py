ce1b554b97bc73f4e9c31e606e5ac0ad4e10d8eb
Ben Weeder <weederb23@gmail.com>
2019-10-09 15:39:32 -0700

    move re-used functions to common function file

--- data_extraction/scripts/AntiJen_T_Cell_Epitope_extraction.py
+++ data_extraction/scripts/AntiJen_T_Cell_Epitope_extraction.py
@@ -26,92 +26,24 @@ Outputs:
 import urllib.request
 import pandas as pd
 import numpy as np
+import extraction_functions as ef
 
 file_name = "T C2.csv"
 
 """ Data Extraction """
 df = pd.read_csv(file_name)[["Description"]]
-
 print(df.shape)
 
 
-def f(x):
-    """Obtains the script of the page related to the given description
-       Arguments:
-           x (str): a certain epitope description in the dataframe
-       Returns:
-           str: the full script of the page
-    """
-    with urllib.request.urlopen("http://www.ddg-pharmfac.net/antijen/scripts/"
-                                + "aj_scripts/aj_tcellcalc2.pl?epitope="
-                                + x + "&AL=%25&ST=%25&CAT="
-                                + "T+Cell+Epitope") as h:
-        return str(h.read())
+df["Buffer"] = df["Description"].apply(ef.get_script_page)
+df["Allele Name"] = df.apply(ef.get_alleles, axis=1)
+df["Parent Protein IRI"] = df["Buffer"].apply(ef.get_sprot_IRI)
+df['IRI_type'] = "Uniprot"
+df["MHC"] = df.apply(ef.get_mhc_types, axis=1)
 
-
-df["Buffer"] = df["Description"].apply(f)
-
-
-def get_alleles(x):
-    """Obtains the allele types of a certain epitope using the script
-       Arguments:
-           x (int): the directory of the dataframe
-       Returns:
-           str: the allele type(s) of a certain epitope
-    """
-    split_allele = x["Buffer"].split("allele.cgi?")
-    alleles = []
-    for i in range(len(split_allele)):
-        if i != 0:
-            alleles.append(split_allele[i][:6])
-
-    if len(alleles) > 0:
-        alleles = list(dict.fromkeys(alleles))
-        return ", ".join(alleles)
-    else:
-        buffer = split_allele[0].split("</td>\\n\\t<td>CLASS-2")[0].split(">")
-        return buffer[len(buffer) - 1]
-
-
-def get_sprot(buffer):
-    """Obtains the UniProt IRI of the protein an epitope is derived from
-       Arguments:
-           x (int): the directory of the dataframe
-       Returns:
-           str: the UniProt IRI
-    """
-    if len(buffer.split("sprot-entry?")) > 1:
-        return buffer.split("sprot-entry?")[1].split("\"")[0].split("http")[0]
-    else:
-        return np.nan
-
-
-def get_mhc(x):
-    """Obtains the organism name of the MHC species the epitope came from\
-
-         Used to filter for human MHC species later
-
-         Arguments:
-             x (int): the directory of the dataframe
-         Returns:
-             str: the MHC species of the epitope
-      """
-    try:
-        # Manually check any epitopes which may have a HUMAN MHC (just not in the first row)
-        if "HUMAN" in x["Buffer"] and x["Buffer"].split("CLASS-2</td>\\n\\t<td>")[1].split("<")[0] != "HUMAN" and len(x["Buffer"].split("CLASS-1</td>\\n\\t<td>")) > 2:
-            print(x["Description"])
-        return x["Buffer"].split("CLASS-2</td>\\n\\t<td>")[1].split("<")[0]
-    except IndexError:
-        return np.nan
-
-
-df["Allele Name"] = df.apply(get_alleles, axis=1)
-df["Parent Protein IRI (Uniprot)"] = df["Buffer"].apply(get_sprot)
-df["MHC"] = df.apply(get_mhc, axis=1)
-
-df = df[[x == "HUMAN" for x in df["MHC"]]]
+df = df[[m == "HUMAN" for m in df["MHC"]]]
 
 df.drop(columns=["Buffer", "MHC"], inplace=True)
-df.dropna(subset=["Parent Protein IRI (Uniprot)"], inplace=True)
+df.dropna(subset=["Parent Protein IRI"], inplace=True)
 
 df.to_csv("T_Cell_C2.csv", index=False)
---
+++ data_extraction/scripts/extraction_functions.py
@@ -0,0 +1,78 @@
+#!usr/bin/env python3
+"""
+extraction_functions.py
+
+For issues contact Ben Weeder (weeder@ohsu.edu)
+
+This script consolidates functions previously written for use in epitope and
+cleavage site extraction scripts.
+"""
+import urllib.request
+import numpy as np
+
+
+def get_script_page(x):
+    """Obtains the script of the page related to the given description
+       Arguments:
+           x (str): a certain epitope description in the dataframe
+       Returns:
+           str: the full script of the page
+    """
+    with urllib.request.urlopen("http://www.ddg-pharmfac.net/antijen/scripts/"
+                                + "aj_scripts/aj_tcellcalc2.pl?epitope="
+                                + x + "&AL=%25&ST=%25&CAT="
+                                + "T+Cell+Epitope") as h:
+        return str(h.read())
+
+
+def get_alleles(x):
+    """Obtains the allele types of a certain epitope using the script
+       Arguments:
+           x (int): the directory of the dataframe
+       Returns:
+           str: the allele type(s) of a certain epitope
+    """
+    split_allele = x["Buffer"].split("allele.cgi?")
+    alleles = []
+    for i in range(len(split_allele)):
+        if i != 0:
+            alleles.append(split_allele[i][:6])
+
+    if len(alleles) > 0:
+        alleles = list(dict.fromkeys(alleles))
+        return ", ".join(alleles)
+    else:
+        buffer = split_allele[0].split("</td>\\n\\t<td>CLASS-2")[0].split(">")
+        return buffer[len(buffer) - 1]
+
+
+def get_sprot_IRI(buffer):
+    """Obtains the UniProt IRI of the protein an epitope is derived from
+       Arguments:
+           x (int): the directory of the dataframe
+       Returns:
+           str: the UniProt IRI
+    """
+    if len(buffer.split("sprot-entry?")) > 1:
+        return buffer.split("sprot-entry?")[1].split("\"")[0].split("http")[0]
+    else:
+        return np.nan
+
+
+def get_mhc_types(x):
+    """Obtains the organism name of the MHC species the epitope came from\
+
+         Used to filter for human MHC species later
+
+         Arguments:
+             x (int): the directory of the dataframe
+         Returns:
+             str: the MHC species of the epitope
+      """
+    try:
+        # Manually check any epitopes which may have a HUMAN MHC (just not in the first row)
+        if "HUMAN" in x["Buffer"] and x["Buffer"].split("CLASS-2</td>\\n\\t<td>")[1].split("<")[0] != "HUMAN" and len(x["Buffer"].split("CLASS-1</td>\\n\\t<td>")) > 2:
+            print(x["Description"])
+        return x["Buffer"].split("CLASS-2</td>\\n\\t<td>")[1].split("<")[0]
+    except IndexError:
+        return np.nan
