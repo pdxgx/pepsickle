245eb2d7ff24d36641a9ab8952265e111e790cf7
Ben Weeder <weederb23@gmail.com>
2019-10-09 11:44:34 -0700

    adds code for merging pos and neg. data. Allows annotation of type and source

--- data_processing/scripts/merge_datasets.py
+++ data_processing/scripts/merge_datasets.py
@@ -14,3 +14,41 @@ Input:
 Output:
 - a single merged .csv with consistent column headers
 """
+import os
+import re
+import pandas as pd
+
+# Later replace file_dir with -i argparse
+file_dir = os.getcwd() + "/../pre-merged_data/"
+# get lists of positive and negative files to load in
+pos_data_files = os.listdir(file_dir + "positives/")
+neg_data_files = os.listdir(file_dir + "negatives/")
+
+# compile all files into one list
+data_files = [file_dir + "positives/" + pf for pf in pos_data_files]
+for nf in neg_data_files:
+    data_files.append(file_dir + "negatives/" + nf)
+
+# annotate the type of example (pos or neg)
+source_type = ["pos"] * len(pos_data_files) + ['neg'] * len(neg_data_files)
+
+# strip file paths to retrieve base names
+source_names = [re.sub("\..*","", re.sub(".*/", "", f)) for f in data_files]
+
+# initialize with first data set
+merged_df_raw = pd.read_csv(data_files[0], low_memory=False)
+# add row with source database
+merged_df_raw['data_source'] = source_names[0]
+merged_df_raw['example_type'] = source_type[0]
+
+# loop through and repeat for remaining files
+for i in range(1, len(data_files)):
+    # load in single file and add necessary columns
+    tmp_df = pd.read_csv(data_files[i], low_memory=False)
+    tmp_df['data_source'] = source_names[i]
+    tmp_df['example_type'] = source_type[i]
+    # append onto initiated df
+    merged_df_raw = merged_df_raw.append(tmp_df, sort=False)
+    del tmp_df
+
+
