bb710433e657f17e926dbc55f11fa8a1de5a547f
Ben Weeder <weederb23@gmail.com>
2019-10-30 09:26:42 -0700

    remove code for VHSE S1 and S3 (redundancies from AntiJen and IEDB)

--- data_extraction/scripts/extract_VHSE_data.py
+++ data_extraction/scripts/extract_VHSE_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env Python3
 """
-
+doi: 10.1371/journal.pone.0074506
 """
 import pandas as pd
 import numpy as np
@@ -8,64 +8,6 @@ import numpy as np
 indir = "/Users/weeder/PycharmProjects/proteasome/data_extraction/" \
         "raw_data/VHSE_data/"
 
-s1_df = pd.read_excel(indir + "Dataset_s1.xlsx")
-s3_df = pd.read_excel(indir + "Dataset_s3.xls")
 s5_df = pd.read_excel(indir + "Dataset_s5.xlsx")
 # s7 relevant for digesion map data?
 
-# initiate data frame
-s1_cleaned_df = pd.DataFrame(columns=["Epitope", "MHC_types", "Species",
-                                      "Categories", "Protein_refs",
-                                      "Ref_type", "Journal_refs"])
-for e in range(len(s1_df)):
-    # pull entries one at a time
-    entry = s1_df.iloc[e]
-
-    # for each new entry... (blank if added info for prev. epitope)
-    if entry['Epitope'] is not np.nan:
-        # pull primary entry + added info
-        entry_n = entry['Number of results']
-        tmp_df = s1_df.iloc[e:int(e+entry_n)]
-
-        # make empty lists
-        Class = []
-        Species = []
-        Category = []
-        Protein_ref = []
-        Journal_ref = []
-        # append entries if novel
-        for i in range(len(tmp_df)):
-            dat = tmp_df.iloc[i]
-            if dat['Class'] not in Class:
-                Class.append(str(dat['Class']))
-            if dat['MHC species'] not in Species:
-                Species.append(str(dat['MHC species']))
-            if dat['Category'] not in Category:
-                Category.append(str(dat['Category']))
-            if dat['Swiss Prot Ref'] not in Protein_ref:
-                Protein_ref.append(str(dat['Swiss Prot Ref']))
-            if dat['Journal Ref'] not in Journal_ref:
-                Journal_ref.append(str(dat['Journal Ref']))
-
-        # generate single entries separated with semicolons
-        epitope = entry['Epitope']
-        MHC_types = "; ".join(Class)
-        MHC_species = "; ".join(Species)
-        Categories = "; ".join(Category)
-        Protein_ref = "; ".join(Protein_ref)
-        Ref_type = "Uniprot"  # define reference database used
-        Journal_refs = "; ".join(Journal_ref)
-
-        # compile into new entry
-        new_entry = pd.Series([epitope, MHC_types, MHC_species, Categories,
-                              Protein_ref, Ref_type, Journal_refs],
-                              index=s1_cleaned_df.columns)
-        # append to out_df
-        s1_cleaned_df = s1_cleaned_df.append(new_entry, ignore_index=True)
-
-
-# table S3 processing
-# first drop completely redundant rows
-s3_df.drop_duplicates(inplace=True)
-
-# some duplicated sequences still, but rows are unique... looks like p mods?
