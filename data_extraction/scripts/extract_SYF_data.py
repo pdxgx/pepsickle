c6056550beff34575b910e9f3c2217f24ff975f8
Ben Weeder <weederb23@gmail.com>
2019-11-12 09:55:28 -0800

    updates to pull name and id for source proteins

--- data_extraction/scripts/extract_SYF_data.py
+++ data_extraction/scripts/extract_SYF_data.py
@@ -3,22 +3,30 @@
 This script extracts the epitope/ligand information from the SYFPEITHI
 database (http://www.syfpeithi.de) and converts it into a csv file
 """
-
 import pandas as pd
 import extraction_functions as ef
+import os
 
+indir = "/Users/weeder/PycharmProjects/proteasome/data_extraction/raw_data/"
 outdir = "/Users/weeder/PycharmProjects/proteasome/data_processing/" \
          "un-merged_data/positives/"
 
 # NOTE: Pulls all epitopes, for class I & II and different organisms... need
 # to restrict this further to save time/downstream filtering issues...
-raw_allele_list = ef.get_SYF_alleles()
-allele_list = raw_allele_list[2:]
+# raw_allele_list = ef.get_SYF_alleles()
+# allele_series = pd.Series(raw_allele_list)
+# allele_series.to_csv("../raw_allele_series.csv", index=False, header=False)
 
-full_SYF_df = pd.DataFrame(columns=['epitope', 'source',
+mammal_allele_df = pd.read_csv(indir + "raw_mammal_allele_series.csv")
+mammal_allele_df = mammal_allele_df[mammal_allele_df.Class != "II"]
+
+allele_list = mammal_allele_df.Allele
+
+full_SYF_df = pd.DataFrame(columns=['epitope', 'prot_name', 'ebi_id'
                                     'reference', 'allele'])
 
 for a in allele_list:
+    print(a)
     allele_query = ef.compile_SYF_url(a)
     try:
         tmp_df = ef.extract_SYF_table(allele_query)
@@ -26,8 +34,7 @@ for a in allele_list:
         continue
     if len(tmp_df) > 0:
         tmp_df['allele'] = a
-        full_SYF_df = full_SYF_df.append(tmp_df, ignore_index=True)
+        full_SYF_df = full_SYF_df.append(tmp_df, ignore_index=True, sort=True)
 
-print(full_SYF_df)
 
-# full_SYF_df.to_csv(outdir+"SYFPEITHI_epitopes.csv", index=False)
+full_SYF_df.to_csv(outdir+"SYFPEITHI_epitopes.csv", index=False)
