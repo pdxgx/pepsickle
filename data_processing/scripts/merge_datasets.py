65427dbd63f9c86817a377d5e3326512edabe5dd
Ben Weeder <weederb23@gmail.com>
2019-12-16 10:29:23 -0800

    fixes for properly checking sequence indexes, new script for extracting digestion map data, updated merge and updated sequence featurization tools.

--- data_processing/scripts/merge_datasets.py
+++ data_processing/scripts/merge_datasets.py
@@ -19,7 +19,7 @@ import re
 import pandas as pd
 
 # Later replace file_dir with -i argparse
-file_dir = os.getcwd() + "/../un-merged_data/"
+file_dir = "/Users/weeder/PycharmProjects/proteasome/data_processing/un-merged_data/"
 # get lists of positive and negative files to load in
 pos_data_files = os.listdir(file_dir + "positives/")
 neg_data_files = os.listdir(file_dir + "negatives/")
--- extra_code/epitope_index_check.py
+++ extra_code/epitope_index_check.py
@@ -1,70 +1,104 @@
 import re
+import pandas as pd
 # loop for checking all index cases and repairing indices when possible
 # replace df with the full df of positive epitope examples
+file_dir = "/Users/weeder/PycharmProjects/proteasome/data_processing/" \
+           "un-merged_data/"
+
+df = pd.read_csv(file_dir + 'tmp_merged.csv')
+df = df.where(pd.notnull(df), None)
+df = df[df.full_sequence.notnull()]
+df = df.reset_index(drop=True)
+
 failed_indices = []
 for i in range(len(df)):
     # pull out a single row
     row = df.iloc[i]
     # convert the epitope into a regular expression for seq. search
-    search_exp = create_sequence_regex(row['linear_peptide_seq'])
+    search_exp = create_sequence_regex(row['fragment'])
     # if there's a starting and ending position...
-    if row['starting_position'] and row['ending_position']:
+    if row['start_pos'] and row['end_pos']:
         # get start & stop, these may need shifted depending on ref. indexing
-        start = int(row['starting_position']) - 1  # if base 0
-        end = int(row['ending_position'])
+        start = int(row['start_pos']) - 1  # if base 0
+        end = int(row['end_pos'])
         # pull out expected match from source
-        source_seq = str(row['sequence'][start:end])
+        source_seq = str(row['full_sequence'][start:end])
         # if the sequences match, go to next row iteration
         if re.match(search_exp, source_seq):
+            df.at[i, 'start_pos'] = start
             continue
         # if doesn't match, but only occurs once in origin sequence...
-        elif len(re.findall(search_exp, str(row['sequence']))) == 1:
+        elif len(re.findall(search_exp, str(row['full_sequence']))) == 1:
             # find location
-            s = re.search(search_exp, str(row['sequence']))
+            s = re.search(search_exp, str(row['full_sequence']))
             # update start position with start of match
-            df.iloc[i]['starting_position'] = s.span()[0]
+            df.at[i, 'start_pos'] = s.span()[0]
             # update end position with start of match
-            df.iloc[i]['ending_position'] = s.span()[1]
+            df.at[i, 'end_pos'] = s.span()[1]
         # if 0 or >1 matches, fail
         else:
             failed_indices.append(i)
     # either start or end is missing but not both
-    elif row['starting_position'] or row['ending_position']:
+    elif row['start_pos'] or row['end_pos']:
         # if start position is present
-        if row['starting_position']:
-            start = int(row['starting_position']) - 1  # if 0 base
-            end = int(row['starting_position']) + \
-                  len(row['linear_peptide_seq'])
+        if row['start_pos']:
+            start = int(row['start_pos']) - 1  # if 0 base
+            end = int(row['start_pos']) + \
+                  len(row['fragment'])
         # if end position is present
-        if row['ending_position']:
-            end = int(row['ending_position'])
-            start = end - len(row['linear_peptide_seq']) - 1  # if 0 base
+        if row['end_pos']:
+            end = int(row['end_pos'])
+            start = end - len(row['fragment']) - 1  # if 0 base
         # with newly defined positions, repeat code block from above
-        source_seq = str(row['sequence'][start:end])
+        source_seq = str(row['fragment'][start:end])
         # if the sequences match, go to next row iteration
         if re.match(search_exp, source_seq):
+            df.at[i, 'start_pos'] = start
+            df.at[i, 'end_pos'] = end
             continue
         # if doesn't match, but only occurs once in origin sequence...
-        elif len(re.findall(search_exp, str(row['sequence']))) == 1:
+        elif len(re.findall(search_exp, str(row['full_sequence']))) == 1:
             # find location
-            s = re.search(search_exp, str(row['sequence']))
+            s = re.search(search_exp, str(row['full_sequence']))
             # update start position with start of match
-            df.iloc[i]['starting_position'] = s.span()[0]
+            df.at[i, 'start_pos'] = s.span()[0]
             # update end position with start of match
-            df.iloc[i]['ending_position'] = s.span()[1]
+            df.at[i, 'end_pos'] = s.span()[1]
         # if 0 or >1 matches, fail
         else:
             failed_indices.append(i)
     # if no start and no end position
     else:
         # if only one occurrence...
-        if len(re.findall(search_exp, str(row['sequence']))) == 1:
+        if len(re.findall(search_exp, str(row['full_sequence']))) == 1:
             # find location
-            s = re.search(search_exp, str(row['sequence']))
+            s = re.search(search_exp, str(row['full_sequence']))
             # update start position with start of match
-            df.iloc[i]['starting_position'] = s.span()[0]
+            df.at[i, 'start_pos'] = s.span()[0]
             # update end position with start of match
-            df.iloc[i]['ending_position'] = s.span()[1]
+            df.at[i, 'end_pos'] = s.span()[1]
         # if 0 or >1 matches, fail
         else:
             failed_indices.append(i)
+
+len(failed_indices)
+df.drop(index=failed_indices, inplace=True)
+df = df.reset_index(drop=True)
+
+# double check...
+mismatch_indices = []
+for i in range(len(df)):
+    row = df.iloc[i]
+    start = int(row['start_pos'])
+    end = int(row['end_pos'])
+    source_seq = str(row['full_sequence'][start:end])
+    if str(row['fragment']) == source_seq:
+        pass
+    else:
+        mismatch_indices.append(i)
+
+# for now, drop few with errors:
+df.drop(index=mismatch_indices, inplace=True)
+df = df.reset_index(drop=True)
+
+df.to_csv(file_dir+"tmp_data_indices_repaired.csv", index=False)
