8ef15e3314f2309f56e5c9dcbef782d1dc595e95
Ben Weeder <weederb23@gmail.com>
2021-03-10 11:52:37 -0800

    updates tests for ML models

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -24,7 +24,8 @@ class TestExactModelOutput(unittest.TestCase):
 
         :return:
         """
-        self.seq = ["MPLEQRSQHCKPE"]
+        self.seq_full = ["MPLEQRSQHCKPEMPLE"]
+        self.seq_short = ["MPLEQRS"]
 
     def test_epitope_model(self):
         """
@@ -33,28 +34,32 @@ class TestExactModelOutput(unittest.TestCase):
         """
         self.setUP()
         cleavage_model = initialize_epitope_model()
-        feature_set = sft.generate_feature_array(self.seq)
-
+        # assert cleavage mod type is expected
+        feature_set = sft.generate_feature_array(self.seq_full)
         pred = predict_epitope_mod(cleavage_model, feature_set)
-        self.assertAlmostEqual(round(pred[0], 5), round(0.18569, 5))
+        self.assertAlmostEqual(round(pred[0], 5), round(0.41893, 5))
 
     def test_constitutive_digestion_model(self):
         self.setUP()
-        cleavage_model = initialize_digestion_model()
-        feature_set = sft.generate_feature_array(self.seq)
+        cleavage_model = initialize_digestion_gb_model()
+        # assert cleavage mod type is expected
+        feature_set = sft.generate_feature_array(self.seq_short,
+                                                 normalize=True)
 
-        pred = predict_digestion_mod(cleavage_model, feature_set,
+        pred = predict_digestion_gb_mod(cleavage_model, feature_set,
                                      proteasome_type="C")
-        self.assertAlmostEqual(round(pred[0], 5), 0.99859)
+        self.assertAlmostEqual(round(pred[0], 5), 0.46193)
 
     def test_immuno_digestion_model(self):
         self.setUP()
         cleavage_model = initialize_digestion_model()
-        feature_set = sft.generate_feature_array(self.seq)
+        # assert cleavage mod type is expected
+        # self.assertIsInstance(cleavage_model, CLASS_NAME)
+        feature_set = sft.generate_feature_array(self.seq_short)
 
         pred = predict_digestion_mod(cleavage_model, feature_set,
                                      proteasome_type="I")
-        self.assertAlmostEqual(round(pred[0], 5), 0.78628)
+        self.assertAlmostEqual(round(pred[0], 5), 0.7441)
 
     def test_epitope_model_human(self):
         """
@@ -63,26 +68,26 @@ class TestExactModelOutput(unittest.TestCase):
         """
         self.setUP()
         cleavage_model = initialize_epitope_model(human_only=True)
-        feature_set = sft.generate_feature_array(self.seq)
+        feature_set = sft.generate_feature_array(self.seq_full)
 
         pred = predict_epitope_mod(cleavage_model, feature_set)
-        self.assertAlmostEqual(round(pred[0], 5), 0.18569)
+        self.assertAlmostEqual(round(pred[0], 5), 0.58982)
 
     def test_constitutive_digestion_model_human(self):
         self.setUP()
         cleavage_model = initialize_digestion_model(human_only=True)
-        feature_set = sft.generate_feature_array(self.seq)
+        feature_set = sft.generate_feature_array(self.seq_short)
 
         pred = predict_digestion_mod(cleavage_model, feature_set, proteasome_type="C")
-        self.assertAlmostEqual(round(pred[0], 5), 0.99778)
+        self.assertAlmostEqual(round(pred[0], 5), 0.79977)
 
     def test_immuno_digestion_model_human(self):
         self.setUP()
         cleavage_model = initialize_digestion_model(human_only=True)
-        feature_set = sft.generate_feature_array(self.seq)
+        feature_set = sft.generate_feature_array(self.seq_short)
 
         pred = predict_digestion_mod(cleavage_model, feature_set, proteasome_type="I")
-        self.assertAlmostEqual(round(pred[0], 5), 0.00309)
+        self.assertAlmostEqual(round(pred[0], 5), 0.3908)
 
 
 class TestSequenceProcessing(unittest.TestCase):
@@ -111,7 +116,6 @@ class TestSequenceProcessing(unittest.TestCase):
         """
         self.setUP()
         cleavage_model = initialize_epitope_model()
-        self.assertIsInstance(cleavage_model, epitopeFullNet)
         out_df = predict_protein_cleavage_locations(protein_id="None",
                                                     protein_seq=self.seq,
                                                     model=cleavage_model,
@@ -119,7 +123,7 @@ class TestSequenceProcessing(unittest.TestCase):
                                                     proteasome_type="E")
         for i, entry in enumerate(out_df):
             self.assertEqual(len(entry), 4)
-        self.assertEqual(i, 312)
+        self.assertEqual(i+1, len(self.seq))
 
     def test_constitutive_digestion_model(self):
         """
@@ -128,17 +132,16 @@ class TestSequenceProcessing(unittest.TestCase):
 
         """
         self.setUP()
-        cleavage_model = initialize_digestion_model()
-        self.assertIsInstance(cleavage_model, digestionFullNet)
+        cleavage_model = initialize_digestion_gb_model()
         out_df = predict_protein_cleavage_locations(protein_id="None",
                                                     protein_seq=self.seq,
                                                     model=cleavage_model,
-                                                    mod_type="digestion",
+                                                    mod_type="in-vitro",
                                                     proteasome_type="C")
 
         for i, entry in enumerate(out_df):
             self.assertEqual(len(entry), 4)
-        self.assertEqual(i, 312)
+        self.assertEqual(i+1, len(self.seq))
 
     def test_immuno_digestion_model(self):
         """
@@ -147,16 +150,15 @@ class TestSequenceProcessing(unittest.TestCase):
 
         """
         self.setUP()
-        cleavage_model = initialize_digestion_model()
-        self.assertIsInstance(cleavage_model, digestionFullNet)
+        cleavage_model = initialize_digestion_gb_model()
         out_df = predict_protein_cleavage_locations(protein_id="None",
                                                     protein_seq=self.seq,
                                                     model=cleavage_model,
-                                                    mod_type="digestion",
+                                                    mod_type="in-vitro",
                                                     proteasome_type="I")
         for i, entry in enumerate(out_df):
             self.assertEqual(len(entry), 4)
-        self.assertEqual(i, 312)
+        self.assertEqual(i+1, len(self.seq))
 
     def test_epitope_model_human(self):
         """
@@ -166,14 +168,13 @@ class TestSequenceProcessing(unittest.TestCase):
         """
         self.setUP()
         cleavage_model = initialize_epitope_model(human_only=True)
-        self.assertIsInstance(cleavage_model, epitopeFullNet)
         out_df = predict_protein_cleavage_locations(protein_id="None",
                                                     protein_seq=self.seq,
                                                     model=cleavage_model,
                                                     mod_type="epitope")
         for i, entry in enumerate(out_df):
             self.assertEqual(len(entry), 4)
-        self.assertEqual(i, 312)
+        self.assertEqual(i+1, len(self.seq))
 
     def test_constitutive_digestion_model_human(self):
         """
@@ -182,16 +183,15 @@ class TestSequenceProcessing(unittest.TestCase):
 
         """
         self.setUP()
-        cleavage_model = initialize_digestion_model(human_only=True)
-        self.assertIsInstance(cleavage_model, digestionFullNet)
+        cleavage_model = initialize_digestion_gb_model(human_only=True)
         out_df = predict_protein_cleavage_locations(protein_id="None",
                                                     protein_seq=self.seq,
                                                     model=cleavage_model,
-                                                    mod_type="digestion",
+                                                    mod_type="in-vitro",
                                                     proteasome_type="C")
         for i, entry in enumerate(out_df):
             self.assertEqual(len(entry), 4)
-        self.assertEqual(i, 312)
+        self.assertEqual(i+1, len(self.seq))
 
     def test_immuno_digestion_model_human(self):
         """
@@ -200,16 +200,15 @@ class TestSequenceProcessing(unittest.TestCase):
 
         """
         self.setUP()
-        cleavage_model = initialize_digestion_model(human_only=True)
-        self.assertIsInstance(cleavage_model, digestionFullNet)
+        cleavage_model = initialize_digestion_gb_model(human_only=True)
         out_df = predict_protein_cleavage_locations(protein_id="None",
                                                     protein_seq=self.seq,
                                                     model=cleavage_model,
-                                                    mod_type="digestion",
+                                                    mod_type="in-vitro",
                                                     proteasome_type="I")
         for i, entry in enumerate(out_df):
             self.assertEqual(len(entry), 4)
-        self.assertEqual(i, 312)
+        self.assertEqual(i+1, len(self.seq))
 
 
 class TestFastaProcessing(unittest.TestCase):
@@ -235,10 +234,9 @@ class TestFastaProcessing(unittest.TestCase):
         """
         self.setUP()
         cleavage_model = initialize_epitope_model()
-        self.assertIsInstance(cleavage_model, epitopeFullNet)
         out_df = process_fasta(self.fasta,
                                cleavage_model)
-        self.assertEqual(len(out_df), 314)
+        self.assertEqual(len(out_df)-1, 314)
 
     def test_constitutive_digestion_model(self):
         """
@@ -247,13 +245,12 @@ class TestFastaProcessing(unittest.TestCase):
 
         """
         self.setUP()
-        cleavage_model = initialize_digestion_model()
-        self.assertIsInstance(cleavage_model, digestionFullNet)
+        cleavage_model = initialize_digestion_gb_model()
         out_df = process_fasta(fasta_file=self.fasta,
                                cleavage_model=cleavage_model,
-                               mod_type="digestion",
+                               mod_type="in-vitro",
                                proteasome_type="C")
-        self.assertEqual(len(out_df), 314)
+        self.assertEqual(len(out_df)-1, 314)
 
     def test_immuno_digestion_model(self):
         """
@@ -262,13 +259,12 @@ class TestFastaProcessing(unittest.TestCase):
 
         """
         self.setUP()
-        cleavage_model = initialize_digestion_model()
-        self.assertIsInstance(cleavage_model, digestionFullNet)
+        cleavage_model = initialize_digestion_gb_model()
         out_df = process_fasta(fasta_file=self.fasta,
                                cleavage_model=cleavage_model,
-                               mod_type="digestion",
+                               mod_type="in-vitro",
                                proteasome_type="I")
-        self.assertEqual(len(out_df), 314)
+        self.assertEqual(len(out_df)-1, 314)
 
 
 class testFileOutput(unittest.TestCase):
